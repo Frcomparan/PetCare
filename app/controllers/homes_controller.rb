52cfbb1716af41051a44bbc10558548741b7c030
DiegoSteve <diegosteve123321@gmail.com>
2022-05-04 18:52:27 -0500

    Finished the mobile layout
    Co-authored-by: brocha05 <brocha_05@hotmail.com>

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomesController < ApplicationController
-  before_action :authenticate_user!, except: %i[index]
+  before_action :authenticate_user!, except: %i[index show]
   load_and_authorize_resource
 
   # GET /homes or /homes.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :homes
+  resources :homes, except: [:delete]
   resources :pets
+
+  get '/homes/:id/delete' => 'homes#destroy', as: 'homes_delete'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
