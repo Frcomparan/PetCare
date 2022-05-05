1160cfed710badbe7f13cb9856785b81947e6852
Frcomparan <frcomparan@gmail.com>
2022-05-05 18:47:05 -0500

    Add function to deleted photos in home post

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
   resources :homes
   resources :pets
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "pages#home"
 end
