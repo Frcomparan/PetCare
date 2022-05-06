23531e376320b766e57d8464c8b9d42a24e40f5c
Frcomparan <frcomparan@gmail.com>
2022-05-05 19:24:45 -0500

    Avoid see multiple photos on upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
-  class UsersController < ApplicationController
-    before_action :authenticate_user!, except: %i[show]
-    load_and_authorize_resource
-  
-    def show
-    end
-  
-    def profile
-      @user = current_user
-    end
+  before_action :authenticate_user!, except: %i[show]
+  load_and_authorize_resource
+
+  def show
+  end
+
+  def profile
+    @user = current_user
   end
 end
---
+++ app/javascript/functionalities.js
@@ -0,0 +1,42 @@
+const showGeneralMenu = () => {
+  document.getElementById("general-menu").classList.toggle("show-menu");
+}
+
+const showUserMenu = () => {
+  document.getElementById("session-menu").classList.toggle("show-menu");
+}
+
+const loadFile = (event) => {
+	let img = document.getElementById('output');
+  document.getElementById('add').style.display = 'none';
+	img.src = URL.createObjectURL(event.target.files[0]);
+  img.style.display = 'block'
+};
+
+const loadFiles = (event) => {
+	let container = document.getElementById('upload');
+  document.getElementById('add').style.display = 'none';
+  const uploadPhotos = event.target.files;
+  for (let key = 0; key < uploadPhotos['length']; key++) {
+    img = '<img class="photo" id="output' + key + '"/>';
+    container.innerHTML = container.innerHTML + img;
+    img = document.getElementById('output' + key)
+    img.src = URL.createObjectURL(uploadPhotos[key]);
+    img.style.display = 'block'
+  }
+};
+
+window.onclick = function(event) {
+  let filter_validation =  (event.target.matches('.filter-options') || event.target.matches('.fa-bars'));
+  let user_validation = (event.target.matches('.user-options') || event.target.matches('.fa-user'));
+  if (!(user_validation || filter_validation)) {
+    let dropdowns = document.getElementsByClassName("menu");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show-menu')) {
+        openDropdown.classList.remove('show-menu');
+      }
+    }
+  }
+}
