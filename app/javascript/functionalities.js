e5828747891e0e64c38d676d4db96ffdb09d6183
Frcomparan <frcomparan@gmail.com>
2022-05-06 11:12:05 -0500

    Improve JS code for uploaded photos

--- app/javascript/functionalities.js
+++ app/javascript/functionalities.js
@@ -1,33 +1,49 @@
 const showGeneralMenu = () => {
   document.getElementById("general-menu").classList.toggle("show-menu");
-}
+};
 
 const showUserMenu = () => {
   document.getElementById("session-menu").classList.toggle("show-menu");
-}
+};
 
 const loadFile = (event) => {
-	let img = document.getElementById('output');
+  let img = document.getElementById('output');
   document.getElementById('add').style.display = 'none';
-	img.src = URL.createObjectURL(event.target.files[0]);
+  img.src = URL.createObjectURL(event.target.files[0]);
   img.style.display = 'block'
 };
 
 const loadFiles = (event) => {
-	let container = document.getElementById('upload');
+  let container = document.getElementById('upload');
   document.getElementById('add').style.display = 'none';
   const uploadPhotos = event.target.files;
+  if (document.getElementsByClassName('output').length > 0) {
+    removeChilds(document.getElementsByClassName('output'));
+  }
+  addImages(uploadPhotos, container);
+};
+
+const removeChilds = (imgs) => {
+  imgs = document.getElementsByClassName('output');
+  let parent = imgs[0].parentNode;
+  let size = imgs.length;
+  for (let key = 0; key <= size; key++) {
+    parent.removeChild(parent.lastChild);
+  }
+};
+
+const addImages = (uploadPhotos, container) => {
   for (let key = 0; key < uploadPhotos['length']; key++) {
-    img = '<img class="photo" id="output' + key + '"/>';
+    let img = '<img class="photo output"/>';
     container.innerHTML = container.innerHTML + img;
-    img = document.getElementById('output' + key)
-    img.src = URL.createObjectURL(uploadPhotos[key]);
-    img.style.display = 'block'
+    img = document.getElementsByClassName('output')
+    img[key].src = URL.createObjectURL(uploadPhotos[key]);
+    img[key].style.display = 'block'
   }
 };
 
 window.onclick = function(event) {
-  let filter_validation =  (event.target.matches('.filter-options') || event.target.matches('.fa-bars'));
+  let filter_validation = (event.target.matches('.filter-options') || event.target.matches('.fa-bars'));
   let user_validation = (event.target.matches('.user-options') || event.target.matches('.fa-user'));
   if (!(user_validation || filter_validation)) {
     let dropdowns = document.getElementsByClassName("menu");
@@ -39,4 +55,4 @@ window.onclick = function(event) {
       }
     }
   }
-}
+};
