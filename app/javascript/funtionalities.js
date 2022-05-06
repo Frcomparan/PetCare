430db1fe8bdf1894ba4058cb3c3dc678347fabad
Frcomparan <frcomparan@gmail.com>
2022-05-05 19:15:38 -0500

    Add function to see the upload photos

--- app/javascript/funtionalities.js
+++ app/javascript/funtionalities.js
@@ -2,9 +2,16 @@ const showGeneralMenu = () => {
   document.getElementById("general-menu").classList.toggle("show");
 }
 
-const showUserMenu= () => {
+const showUserMenu = () => {
   document.getElementById("session-menu").classList.toggle("show");
 }
+
+const loadFile = (event) => {
+	let image = document.getElementById('output');
+  document.getElementById('add').style.display = 'none';
+	image.src = URL.createObjectURL(event.target.files[0]);
+  image.style.display = 'block'
+};
 
 window.onclick = function(event) {
   let filter_validation =  (event.target.matches('.filter-options') || event.target.matches('.fa-bars'));
