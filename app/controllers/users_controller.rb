931787c7af061b9a833de5ae3ad8d6dbd9ec69c3
Heikel Villar <hev@oxe360.com>
2022-05-09 20:10:25 -0400

    add user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: %i[show]
   load_and_authorize_resource
 
+  def index
+    @users = User.all
+  end
+
   def show
   end
 
