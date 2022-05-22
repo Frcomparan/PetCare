4ef95af436f2fff7dd126e8f748a88b1480fa043
Frcomparan <frcomparan@gmail.com>
2022-05-21 20:24:20 -0500

    Create notification model and controller

---
+++ db/migrate/20220522012017_create_notifications.rb
@@ -0,0 +1,11 @@
+class CreateNotifications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notifications do |t|
+      t.integer :recipient_id
+      t.datetime :read_at
+      t.integer :notifiable_id
+
+      t.timestamps
+    end
+  end
+end
