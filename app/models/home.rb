d7ff8bb019710316aa8ede615edda4559de0b5c9
Frcomparan <frcomparan@gmail.com>
2022-05-23 16:39:34 -0500

    Modify user to destroy his pets and homes with account delete

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,7 +1,7 @@
 class Home < ApplicationRecord
   belongs_to :user
-  has_many :reviews
-  has_many :reservations
+  has_many :reviews, dependent: :destroy
+  has_many :reservations, dependent: :destroy
   has_many_attached :photos
 
   validates :title, :address, :description, :price, presence: true
